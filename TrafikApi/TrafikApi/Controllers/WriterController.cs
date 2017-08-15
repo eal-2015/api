ccd6c71132ac5c8b3cf1d393f55ce4b7b524c5d5
Simon Glashoff <simon@glashoff.dk>
2017-08-15 14:46:13 +0200

    Added writer controller, it can insert a station or measurement to the db

---
+++ TrafikApi/TrafikApi/Controllers/WriterController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrafikApi.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TrafikApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class WriterController : Controller
+    {
+        IMongoCollection<Station> Stationcollection;
+        IMongoCollection<Measurement> Measurementcollection;
+        // POST api/values
+        [HttpGet]
+        public bool InsertStation(string name, int areacode)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase("Flat_test"); // is made if not already there
+
+            Stationcollection = database.GetCollection<Station>("Stations"); // is made if not already there
+
+            Stationcollection.InsertOne(new Station(name, areacode));
+            return true;
+        }
+        // POST api/values
+        [HttpPost]
+        public void InsertMeasurement(string dateTime, string lane, string speed, string length, string type, string gap, string wrongDir, string display, string flash, string stationName)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase("Flat_test"); // is made if not already there
+
+            Measurementcollection = database.GetCollection<Measurement>("Stations"); // is made if not already there
+
+            Measurementcollection.InsertOne(new Measurement(dateTime, lane, speed, length, type, gap, wrongDir, display, flash, stationName));
+        }
+    }
+}
