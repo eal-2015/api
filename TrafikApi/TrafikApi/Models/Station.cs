3dd035ee7ad02cc2a6117c48ce2cb0aabcfb7153
Simon Glashoff <simon@glashoff.dk>
2017-08-23 10:45:13 +0200

    Changed Station object class to better represent station objects

--- TrafikApi/TrafikApi/Models/Station.cs
+++ TrafikApi/TrafikApi/Models/Station.cs
@@ -9,13 +9,25 @@ namespace TrafikApi.Models
     [BsonIgnoreExtraElements]
     public class Station
     {
-        public string name { get; set; }
         public int areacode { get; set; }
+        public string name { get; set; }
+        public DateTime installed { get; set; }
+        public string equipmentType { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
 
-        public Station(string name, int areacode)
+        public Station()
         {
-            this.name = name;
+
+        }
+        public Station(int areacode, string name, DateTime installed, string equipmentType, double latitude, double longitude)
+        {
             this.areacode = areacode;
+            this.name = name;
+            this.installed = installed;
+            this.equipmentType = equipmentType;
+            this.latitude = latitude;
+            this.longitude = longitude;
         }
     }
 }
