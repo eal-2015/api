e22a98b9b9e0248fd5bd28825df93c1bea1bde13
npc <nick_paw@hotmail.com>
2017-08-29 13:41:22 +0200

    Init GetAllStationsAPI + Modelhelper class library

---
+++ TrafikApi/ModelHelper/Mongo.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+
+namespace ModelHelper
+{
+    public class Mongo
+    {
+        /*
+         * How it would be nice if the type of object the collection is could be abstract
+         * and then determined when instantiating the class.
+         * That would be perfect, but <T> say NO!
+         */
+
+        public IMongoCollection<Station> ConnectToStation(string databaseName, string collectionName)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase(databaseName); // is made if not already there
+            return database.GetCollection<Station>(collectionName); // is made if not already there
+        }
+
+        public IMongoCollection<Measurement> ConnectToMeasurement(string databaseName, string collectionName)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase(databaseName); // is made if not already there
+            return database.GetCollection<Measurement>(collectionName); // is made if not already there
+        }
+    }
+}
