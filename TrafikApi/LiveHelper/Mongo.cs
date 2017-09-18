71e3247babd449c4d6044b0048639bcbfba9de3d
npc <nick_paw@hotmail.com>
2017-09-18 15:09:23 +0200

    Live(data from live db) MeasurmentCount API

---
+++ TrafikApi/LiveHelper/Mongo.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+
+namespace LiveHelper
+{
+    public class Mongo
+    {
+        public IMongoCollection<Measurement> ConnectToMeasurement(string databaseName, string collectionName)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database = client.GetDatabase(databaseName); // is made if not already there
+            return database.GetCollection<Measurement>(collectionName); // is made if not already there
+        }
+    }
+}
---
+++ TrafikApi/LiveTrafficMeasurementsCountAPI/Controllers/MeasurementCountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using LiveHelper;
+
+namespace LiveTrafficMeasurementsCountAPI.Controllers
+{
+    // TO DO
+    [EnableCors("AllowAll")]
+    [Route("api/[controller]/[action]")]
+    public class MeasurementCountController : Controller
+    {
+        Mongo conn = new Mongo();
+
+        [HttpGet]
+        [ActionName("GetMeasurements")]
+        // GET: http://localhost:57719/api/MeasurementCount/GetMeasurements?from=2017-08-14%2009:50:40&to=2017-09-15&stationid=46102360
+        public JsonResult GetMeasurements(DateTime from, DateTime to, int stationid)
+        {
+            IMongoCollection<Measurement> collection = conn.ConnectToMeasurement("Trafik_DB", "LiveMeasurements");
+            var filt = Builders<Measurement>.Filter.Where(x => x.stationid == stationid) & Builders<Measurement>.Filter.Where(x => x.datetime > from && x.datetime < to);
+            var CountResult = collection.Find(filt).Count();
+
+            return Json(CountResult);
+        }
+      
+    }
+}
+
+
